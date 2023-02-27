d13820a8e9773305bb9af9fbec7f832d78bacac2
Chikara Saito <s.chikara3110@gmail.com>
2023-02-27 21:56:18 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: "https://csnagu.github.io",
+  site: "https://nagu.dev",
   base: "/",
   markdown: {
     shikiConfig: {
